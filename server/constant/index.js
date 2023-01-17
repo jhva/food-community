7348df820549cd09564f7eb9209105adb3aed4e9
jhva <junghun8158@naver.com>
2023-01-18 00:28:37 +0900

    proxy test2

--- server/constant/index.js
+++ server/constant/index.js
@@ -4,7 +4,8 @@ let passportConfig = {
 };
 
 let corsConfig = {
-  origin: "https://port-0-food-server-1ih8d2glczugkpe.gksl2.cloudtype.app/", // 접근 권한을 부여하는 도메인
+  origin:
+    "https://web-food-community-front-1ih8d2glczugkpe.gksl2.cloudtype.app/", // 접근 권한을 부여하는 도메인
   credentials: true, // 응답 헤더에 Access-Control-Allow-Credentials 추가
   methods: ["GET", "POST", "OPTIONS", "PATCH", "DELETE"],
 };
