6c54c358aff7dbda469fc6d706940ddf2e0c6638
jhva <junghun8158@naver.com>
2023-01-18 10:53:40 +0900

    kakako login 수정 3차

--- server/controller/user/kakaocallback.js
+++ server/controller/user/kakaocallback.js
@@ -14,7 +14,7 @@ module.exports = async (req, res) => {
       // redirect_uri: process.env.KAKAO_REACT_REDIRECT,
     },
   });
-  const kakaoUserInfo = user;
+  const kakaoUserInfo = user?.data?.kakao_account;
   console.log(kakaoUserInfo);
 
   try {
